a07b03d2d4b734ce33b7e9dd08fcf0663722a97f
Felix Zippel <contact@zippelf.com>
2023-11-03 18:49:06 +0100

    Create keybindings to toggle which key for i,n,v mode as cheatsheet

--- lua/custom/custom-vim-settings/plugin/custom-vim-settings.lua
+++ lua/custom/custom-vim-settings/plugin/custom-vim-settings.lua
@@ -21,9 +21,27 @@ vim.opt.colorcolumn = "8"
 
 require('which-key').register {
   ['<leader>p'] = { name = '[P]roject', _ = 'which_key_ignore' },
+  ['<leader>H'] = { name = '[H]elp', _ = 'which_key_ignore' },
 }
 
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move line down"})
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move line up"})
+
+vim.keymap.set("n", "<leader>Hn", function ()
+  require('which-key').show("", {mode = "n", auto = true})
+end, 
+  {desc = "[H]elp for [n]ormal mode"})
+
+
+vim.keymap.set("n", "<leader>Hv", function ()
+  require('which-key').show("", {mode = "v", auto = true})
+end, 
+  {desc = "[H]elp for [v]isual mode"})
+
+
+vim.keymap.set("n", "<leader>Hi", function ()
+  require('which-key').show("", {mode = "i", auto = true})
+end, 
+  {desc = "[H]elp for [i]nsert mode"})
 
