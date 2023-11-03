3c5d20f26a38606573d8f2c3c3666e2aeaddcab8
Felix Zippel <contact@zippelf.com>
2023-11-03 17:35:39 +0100

    Configure catppuccin; install illuminate

--- lua/custom/plugins/catpuccin.lua
+++ lua/custom/plugins/catpuccin.lua
@@ -4,6 +4,28 @@ return {
   priority = 999,
   config = function ()
     vim.cmd.colorscheme "catppuccin"
+    require('catppuccin').setup({
+      flavour = "mocha",
+      integrations = {
+        cmp = true,
+        gitsigns = true,
+        nvimtree = true,
+        treesitter = true,
+        notify = true,
+        mini = {
+          enabled = true,
+          indentscope_color = "",
+        },
+        harpoon = true,
+        lsp_trouble = true,
+        illuminate = {
+          enabled = true,
+          lsp = true
+        }
+      }
+    })
+
+
   end
 
 }
